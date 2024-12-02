acc0d62e53230b6eb7a19675ecbcc90abceb3c75
summerkrd <145993794+summerkrd@users.noreply.github.com>
2024-12-02 12:29:55 +0300

    1001

--- Assets/ProjectHomeWork/Develop/DI/DIContainer.cs
+++ Assets/ProjectHomeWork/Develop/DI/DIContainer.cs
@@ -8,6 +8,13 @@ namespace Assets.ProjectHomeWork.Develop.DI
 {
     public class DIContainer
     {
+        private readonly Dictionary<Type, Registration> _container = new();
+
+        public void RegisterAsSingle<T>(Func<DIContainer, T> Creator)
+        {
+
+        }
+
         public class Registration
         {
             public Func<DIContainer, object> Creator { get; }
