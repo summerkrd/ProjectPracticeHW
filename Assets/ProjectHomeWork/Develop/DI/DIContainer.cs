07eb1d47fa56e37f4d6940130b2b87a786631a1c
summerkrd <145993794+summerkrd@users.noreply.github.com>
2024-12-04 16:42:35 +0300

    DIContainer

--- Assets/ProjectHomeWork/Develop/DI/DIContainer.cs
+++ Assets/ProjectHomeWork/Develop/DI/DIContainer.cs
@@ -1,18 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Assets.ProjectHomeWork.Develop.DI
 {
     public class DIContainer
     {
         private readonly Dictionary<Type, Registration> _container = new();
+        private readonly DIContainer _parent;
+        private readonly List<Type> _requests = new();
+
+        public DIContainer() : this(null) { }
+
+        public DIContainer(DIContainer parent) => _parent = parent;
 
         public void RegisterAsSingle<T>(Func<DIContainer, T> Creator)
         {
+            if (_container.ContainsKey(typeof(T)))
+                throw new InvalidOperationException(typeof(T) + " Alredy register");
 
+            Registration registration = new Registration(container => Creator(container));
+            _container.Add(typeof(T), registration);
+        }
+
+        public T Resolve<T>()
+        {
+            if (_requests.Contains(typeof(T)))
+                throw new InvalidOperationException($"Cycle resolve for {typeof(T)}");
+
+            _requests.Add(typeof(T));
+
+            try
+            {
+                if (_container.TryGetValue(typeof(T), out Registration registration))
+                    return CreateFrom<T>(registration);
+
+                if (_parent != null)
+                    return _parent.Resolve<T>();
+            }
+            finally
+            {
+                _requests.Remove(typeof(T));
+            }
+
+            throw new InvalidOperationException($"Registration for {typeof(T)} not exist");
+        }
+
+        private T CreateFrom<T>(Registration registration)
+        {
+            if (registration.Instance == null && registration.Creator != null)
+                registration.Instance = registration.Creator(this);
+
+            return (T)registration.Instance;
         }
 
         public class Registration
